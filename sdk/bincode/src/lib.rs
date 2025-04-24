f2ddb2763d40006a345901087562802ce99320db
Stas Stepanov <stanislav@ankr.com>
2025-04-24 16:38:48 +0700

    feat(svm): removed unused

--- sdk/bincode/src/lib.rs
+++ sdk/bincode/src/lib.rs
@@ -7,7 +7,7 @@ extern crate alloc;
 use alloc::vec;
 use alloc::vec::Vec;
 use bincode::{
-    config::{Configuration, Fixint, LittleEndian, NoLimit},
+    config::{Configuration, Fixint, LittleEndian},
     enc, error,
 };
 
